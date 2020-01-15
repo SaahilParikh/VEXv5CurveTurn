897ceff0f9b62d8e052e36a07487b0405ee713f9
SaahilParikh <saahilparikh2000@gmail.com>
2020-01-15 15:12:09 -0800

    simpleCurveTurn created

---
+++ simpleCurveTurn.cpp
@@ -0,0 +1,24 @@
+#define MAX_SPEED 127.0
+#define DRIVE_BASE_WIDTH 10.0 // in Inches
+#define TIME_STEP 0.002 //50H
+
+void drive(float Left, float Right) {
+  // Define driveFunction
+  // driveFunction(*args);
+}
+
+float simpleCurveTurnLeft(float radius, float targetHeading, float v=MAX_SPEED,float time=TIME_STEP){
+	float kA = radius/(radius + DRIVE_BASE_WIDTH);
+	float vOuter= v 
+	float vInner=kA*v;
+	drive(vInner, vOuter);
+	wait(timeStep, sec);
+}
+
+float simpleCurveTurnRight(float radius, float targetHeading, float v=MAX_SPEED,float time=TIME_STEP){
+        float kA = radius/(radius + DRIVE_BASE_WIDTH);
+        float vOuter= v 
+        float vInner=kA*v;
+        drive(vOuter, vInner);
+        wait(timeStep, sec);
+}      
