9a9f2c282674778e8b532712ce5c53d6ef9f1d42
SaahilParikh <saahilparikh2000@gmail.com>
2020-01-17 15:37:34 -0800

    fixed silly stuff

--- simpleCurveTurn.cpp
+++ simpleCurveTurn.cpp
@@ -1,24 +1,23 @@
 #define MAX_SPEED 127.0
 #define DRIVE_BASE_WIDTH 10.0 // in Inches
-#define TIME_STEP 0.002 //50H
 
 void drive(float Left, float Right) {
   // Define driveFunction
   // driveFunction(*args);
 }
 
-float simpleCurveTurnLeft(float radius, float targetHeading, float v=MAX_SPEED,float time=TIME_STEP){
+void simpleCurveTurnLeft(float radius, float t, float v=MAX_SPEED){
 	float kA = radius/(radius + DRIVE_BASE_WIDTH);
 	float vOuter= v; 
 	float vInner=kA*v;
 	drive(vInner, vOuter);
-	wait(timeStep, sec);
+	wait(t, sec);
 }
 
-float simpleCurveTurnRight(float radius, float targetHeading, float v=MAX_SPEED,float time=TIME_STEP){
+void simpleCurveTurnRight(float radius, float t, float v=MAX_SPEED){
         float kA = radius/(radius + DRIVE_BASE_WIDTH);
         float vOuter= v; 
         float vInner=kA*v;
         drive(vOuter, vInner);
-        wait(timeStep, sec);
+        wait(t, sec);
 }      
